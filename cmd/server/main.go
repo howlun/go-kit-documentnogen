9229c15ef56c03b832bd9e2f1d995341d1d7ebe7
How Lun Par <howlun.par@tanchonggroup.com>
2019-06-04 10:26:06 +0700

    removed test function in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -190,20 +190,6 @@ func runMain(c *cli.Context) error {
 	return nil
 }
 
-func accessControl(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
-
-		if r.Method == "OPTIONS" {
-			return
-		}
-
-		h.ServeHTTP(w, r)
-	})
-}
-
 func ensureDir(fileName string) error {
 	dirName := filepath.Dir(fileName)
 	if _, serr := os.Stat(dirName); serr != nil {
